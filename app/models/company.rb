e924b61e271a6a7dbb5411920e2ee253394f81f9
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-09-02 15:58:24 +0300

    Done

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,17 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def give_freebie(dev,item_name,value)
+        freebie=Freebie.create(item_name: item_name,value:value)
+        dev.freebies << Freebie
+    end
+
+    def self.earliest
+        self.minimum("founding_year")
+    end
+
+    def self.oldest_company
+        self.where("founding_year=?",self.earliest)
+    end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,14 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        (self.freebies.find{|freebie| freebie.item_name== item_name})? true: false
+    end
+
+    def give_away(dev,freebie)
+        if self.freebies.include?(freebie)
+            freebie.update(dev_id: freebie)
+        end
+    end
 end
